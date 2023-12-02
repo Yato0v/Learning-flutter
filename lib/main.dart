985c2e44769ef111f0d490e18486267e14cc44a8
Yato0v <mineyato098.22@gmail.com>
2023-12-02 03:48:57 -0300

    debug

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   //Usar vírgula no final de cada widith para formatar
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Victor é gay'),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Eu sou Rico'),
+        ),
       ),
     ),
   );
