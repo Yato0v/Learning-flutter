5492f750b70d628305bd420be63d1768eff06296
Yato0v <mineyato098.22@gmail.com>
2023-12-02 16:04:27 -0300

    Add image in the center

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,12 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey[700],
           title: Text('Eu sou Rico'),
         ),
+        body: Center(child: Image(image: NetworkImage('https://p16-capcut-sign-va.ibyteimg.com/tos-maliva-v-be9c48-us/oozeLpCBEt1JAjAmBbJyLI4elOAiznZFCADQDB~tplv-nhvfeczskr-1:250:0.webp?from=1152184139&x-expires=1728906487&x-signature=HSJ%2Bf1uc6HCG7jv6NH88nJyzBaE%3D'),)),
       ),
     ),
   );
